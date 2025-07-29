3f9563a46a223a85a7aed54830fbc652b7fc7369
DrCornyKelp <hieuanh01122004@gmail.com>
2025-07-29 21:31:53 +0700

    hi

--- src/AzCore/Application.cpp
+++ src/AzCore/Application.cpp
@@ -294,6 +294,12 @@ void Application::mainLoop() {
         // 3rd person camera positioning
 
         camera->pos = p_model.trform.pos - camera->forward * camDist + glm::vec3(0.0f, 0.25f, 0.0f);
+        // If camera.y < 0, push it up with relation to the forward vector
+        if (camera->pos.y < 0.0f) {
+            // Find distance between ground and camera on the forward vector
+            float groundDistance = glm::dot(-camera->forward, glm::vec3(0.0f, 1.0f, 0.0f)) * camDist;
+            camera->pos.y = groundDistance;
+        }
 
         // Move the player plush based on WASD keys
 
