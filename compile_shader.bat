a76b7a0e859859574b4271cd3a295447745a9de9
DrCornyKelp <hieuanh01122004@gmail.com>
2025-08-10 07:37:51 +0700

    hello

--- compile_shader.bat
+++ compile_shader.bat
@@ -6,3 +6,5 @@ glslc Shaders/Rasterize/raster.frag -o Shaders/Rasterize/raster.frag.spv
 
 glslc Shaders/Sky/sky.vert -o Shaders/Sky/sky.vert.spv
 glslc Shaders/Sky/sky.frag -o Shaders/Sky/sky.frag.spv
+
+echo Compiling completed!
