cab0a5853436fabe08acbace0d91b94629544032
DrCornyKelp <hieuanh01122004@gmail.com>
2025-08-15 16:54:11 +0700

    hello

--- compile_shader.bat
+++ compile_shader.bat
@@ -4,7 +4,11 @@ REM Rasterization shaders
 glslc Shaders/Rasterize/raster.vert -o Shaders/Rasterize/raster.vert.spv
 glslc Shaders/Rasterize/raster.frag -o Shaders/Rasterize/raster.frag.spv
 
+REM Ineffcient but really cool sky shaders
 glslc Shaders/Sky/sky.vert -o Shaders/Sky/sky.vert.spv
 glslc Shaders/Sky/sky.frag -o Shaders/Sky/sky.frag.spv
+
+REM Compute shaders
+glslc Shaders/Compute/times2.comp -o Shaders/Compute/times2.comp.spv
 
 echo Compiling completed!
