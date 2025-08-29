82bd9c67a4be599928b1622a56efe56f8e00ca9a
DrCornyKelp <hieuanh01122004@gmail.com>
2025-08-30 00:41:26 +0700

    hello

--- src/AzVulk/Instance.cpp
+++ src/AzVulk/Instance.cpp
@@ -56,7 +56,7 @@ void Instance::createInstance(const std::vector<const char*>& requiredExtensions
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.pEngineName = "AzVulk Engine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_0;
+    appInfo.apiVersion = VK_API_VERSION_1_1;
 
     VkInstanceCreateInfo createInfo{};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
