37c9df40e6a62ccfb593520f3652af2b3d1be9c9
DrCornyKelp <hieuanh01122004@gmail.com>
2025-09-11 23:34:19 +0700

    descriptor rework

--- include/AzVulk/Descriptor.hpp
+++ include/AzVulk/Descriptor.hpp
@@ -24,27 +24,30 @@ struct DescSets {
     DescSets(DescSets&& other) noexcept;
     DescSets& operator=(DescSets&& other) noexcept;
 
-    VkDevice lDevice = VK_NULL_HANDLE;
     void init(const VkDevice lDevice) { this->lDevice = lDevice; }
 
-    VkDescriptorSetLayout layout = VK_NULL_HANDLE; bool layoutOwned = true;
     void createLayout(const std::vector<VkDescriptorSetLayoutBinding>& bindings);
     void createLayout(const std::vector<LayoutBind>& bindingInfos); // Version using LayoutBind struct
     void borrowLayout(VkDescriptorSetLayout layout);
 
-    VkDescriptorPool pool = VK_NULL_HANDLE; bool poolOwned = true;
     void createPool(const std::vector<VkDescriptorPoolSize>& poolSizes, uint32_t maxSets);
     void borrowPool(VkDescriptorPool pool);
 
-    std::vector<VkDescriptorSet> sets;
     void allocate(uint32_t count=1);
 
+    VkDescriptorSetLayout getLayout() const { return layout; }
+    VkDescriptorPool getPool() const { return pool; }
     VkDescriptorSet get(uint32_t index=0) const {
         return index < sets.size() ? sets[index] : VK_NULL_HANDLE;
     }
 
-    VkDescriptorSetLayout getLayout() const { return layout; }
-    VkDescriptorPool getPool() const { return pool; }
+private:
+    VkDevice lDevice = VK_NULL_HANDLE;
+    std::vector<VkDescriptorSet> sets;
+    VkDescriptorPool      pool        = VK_NULL_HANDLE;
+    bool                  poolOwned   = true;
+    VkDescriptorSetLayout layout      = VK_NULL_HANDLE;
+    bool                  layoutOwned = true;
 
     static VkDescriptorSetLayoutBinding fastBinding(const LayoutBind& binder);
 };
