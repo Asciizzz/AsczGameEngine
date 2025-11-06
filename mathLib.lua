c9f4707bb4530e333bfd95d6c1111edd935bacd8
DrCornyKelp <hieuanh01122004@gmail.com>
2025-11-06 07:42:26 +0700

    small reworks

--- mathLib.lua
+++ mathLib.lua
@@ -1,18 +1,5 @@
--- mathLib.lua
--- A static script library with a simple add function
 
-function vars()
-    return {}
-end
-
-function update()
-    -- Static scripts don't need update
-end
-
--- Add two numbers
-function add(a, b)
-    print("mathLib.add called with a=" .. tostring(a) .. ", b=" .. tostring(b))
-    local result = a + b
-    print("mathLib.add returning " .. tostring(result))
-    return result
+-- Test printing function
+function msg(text)
+    print("[mathLib] msg: " .. text)
 end
--- testCaller.lua
+++ testCaller.lua
@@ -1,29 +1,16 @@
--- testCaller.lua
--- Test script that calls add function from mathLib when 'k' is pressed
 
 function vars()
     return {
-        mathLibHandle = rHandle(),  -- Set this to mathLib's handle in the editor
-        counter = 0.0
+        otherLib = rHandle(),  -- Set this to otherLib's handle in the editor
     }
 end
 
 function update()
-    if kState('k') then
-        -- Get the mathLib script
-        print("A")
-        local mathLib = FS:script(VARS.mathLibHandle)
-        print("B")
+    -- #include
+    local otherLib = FS:script(VARS.otherLib)
 
-        if mathLib then
-            -- Call add function: counter + 1
-            local result = mathLib:call("add", VARS.counter, 1.0)
-            print("Result type: " .. type(result))
-            print("Result value: " .. tostring(result))
-            VARS.counter = result
-            print("Counter: " .. tostring(VARS.counter))
-        else
-            print("Error: Could not load mathLib script!")
-        end
+    if kState('k') then
+        -- Will call the other script function when K is pressed
+        if otherLib then otherLib:call("msg", "K key was pressed!") end
     end
 end
