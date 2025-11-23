e4f76cc5cee141543f8d55f04f75bdc13a3eee2d
DrCornyKelp <hieuanh01122004@gmail.com>
2025-11-24 03:47:31 +0700

    small reworks

---
+++ include/tinyRT/rtMesh.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "tinyRegistry.hpp"
+
+#include "tinyVk/Resource/DataBuffer.hpp"
+#include "tinyVk/Resource/Descriptor.hpp"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "tinyData/tinyMesh.hpp"
+
+namespace tinyRT {
+
+namespace Static3D {
+    struct Data {
+        glm::mat4 model = glm::mat4(1.0f); // Model matrix
+        glm::vec4 other = glm::vec4(0.0f); // Additional data
+
+        static VkVertexInputBindingDescription bindingDesc();
+        static std::vector<VkVertexInputAttributeDescription> attrDescs();
+    };
+
+    struct Range {
+        tinyHandle mesh;
+        uint32_t offset = 0;
+        uint32_t count = 0;
+    };
+};
+
+struct MeshRender3D { // Node component
+    tinyHandle mesh; // Point to mesh resource in registry
+};
+
+struct MeshStatic3D {
+    static constexpr uint32_t MAX_INSTANCES = 100000; // 8mb - more than enough
+
+// ---------------------------------------------------------------
+    MeshStatic3D() = default;
+    void init(const tinyVk::Device* deviceVk, const tinyPool<tinyMesh>* meshPool);
+
+    MeshStatic3D(const MeshStatic3D&) = delete;
+    MeshStatic3D& operator=(const MeshStatic3D&) = delete;
+
+    VkBuffer instaBuffer() const { return instaBuffer_; } // Implicit conversion
+
+    void reset() {
+        instaData_.clear();
+        instaRanges_.clear();
+        tempInstaMap_.clear();
+    }
+
+private:
+    const tinyPool<tinyMesh>* meshPool_ = nullptr;
+
+    tinyVk::DataBuffer instaBuffer_;
+
+    std::vector<Static3D::Data> instaData_;
+    std::vector<Static3D::Range> instaRanges_;
+
+    std::unordered_map<tinyHandle, std::vector<Static3D::Data>> tempInstaMap_;
+};
+
+}
+
+using rtMeshRender3D = tinyRT::MeshRender3D;
+
+using rtMeshStatic3D = tinyRT::MeshStatic3D;
---
+++ src/tinyRT/rtMesh.cpp
@@ -0,0 +1,37 @@
+#include "tinyRT/rtMesh.hpp"
+
+using namespace tinyRT;
+using namespace tinyVk;
+
+VkVertexInputBindingDescription Static3D::Data::bindingDesc() {
+    VkVertexInputBindingDescription bindingDescription{};
+    bindingDescription.binding = 1;
+    bindingDescription.stride = sizeof(Data);
+    bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_INSTANCE;
+    return bindingDescription;
+}
+
+std::vector<VkVertexInputAttributeDescription> Static3D::Data::attrDescs() {
+    std::vector<VkVertexInputAttributeDescription> attribs(5);
+
+    attribs[0] = {3, 1, VK_FORMAT_R32G32B32A32_SFLOAT, offsetof(Data, model) + sizeof(glm::vec4) * 0};
+    attribs[1] = {4, 1, VK_FORMAT_R32G32B32A32_SFLOAT, offsetof(Data, model) + sizeof(glm::vec4) * 1};
+    attribs[2] = {5, 1, VK_FORMAT_R32G32B32A32_SFLOAT, offsetof(Data, model) + sizeof(glm::vec4) * 2};
+    attribs[3] = {6, 1, VK_FORMAT_R32G32B32A32_SFLOAT, offsetof(Data, model) + sizeof(glm::vec4) * 3};
+    attribs[4] = {7, 1, VK_FORMAT_R32G32B32A32_SFLOAT, offsetof(Data, other)};
+    return attribs;
+}
+
+
+
+void MeshStatic3D::init(const tinyVk::Device* deviceVk, const tinyPool<tinyMesh>* meshPool) {
+    meshPool_ = meshPool;
+
+    // Create instance buffer with max size
+    size_t bufferSize = MAX_INSTANCES * sizeof(Static3D::Data);
+    instaBuffer_
+        .setDataSize(bufferSize)
+        .setUsageFlags(BufferUsage::Vertex)
+        .setMemPropFlags(MemProp::HostVisibleAndCoherent)
+        .createBuffer(deviceVk);
+}
